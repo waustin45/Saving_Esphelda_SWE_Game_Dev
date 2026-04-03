90a6c2b579cf8b47f4320f7ed2d3cd8e5548685d
waustin45 <108015666+waustin45@users.noreply.github.com>
2026-04-02 21:19:12 -0400

    added some packs and made the title screen

---
+++ Saving Esphelda/Assets/Scripts/MenuSceneManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void LoadTutorialQuestionScene()
+    {
+        SceneManager.LoadScene("TutorialQuestionScene");
+    }
+    public void LoadSettingsScene()
+    {
+        SceneManager.LoadScene("SettingsScene");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void LoadTutorialScene()
+    {
+        SceneManager.LoadScene("Tutorial_Scene");
+    }
+    public void LoadGameMap()
+    {
+        SceneManager.LoadScene("GameMap");
+    }
+}
