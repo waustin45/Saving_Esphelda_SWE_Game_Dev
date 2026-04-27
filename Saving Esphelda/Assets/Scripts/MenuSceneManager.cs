eab4f9bcef34cb574d2daa3d6c03f8b663ec904a
waustin45 <108015666+waustin45@users.noreply.github.com>
2026-04-26 20:54:10 -0400

    fixed level one

--- Saving Esphelda/Assets/Scripts/MenuSceneManager.cs
+++ Saving Esphelda/Assets/Scripts/MenuSceneManager.cs
@@ -22,6 +22,6 @@ public class MenuSceneManager : MonoBehaviour
     }
     public void LoadGameMap()
     {
-        SceneManager.LoadScene("GameMap");
+        SceneManager.LoadScene("Overworld");
     }
 }
